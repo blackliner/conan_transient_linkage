038d718d13f04850238942d5dc004b189e20dab3
ericLemanissier <ericLemanissier@users.noreply.github.com>
2021-09-09 17:30:55 +0200

    Update build.sh
    this should separate the various issues more cleanly

--- build.sh
+++ build.sh
@@ -4,9 +4,10 @@ set -euxo pipefail
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-conan export "${SCRIPT_DIR}/lib_a"
-conan export "${SCRIPT_DIR}/lib_b"
-conan export "${SCRIPT_DIR}/lib_c"
+conan create "${SCRIPT_DIR}/lib_c" 
+conan create "${SCRIPT_DIR}/lib_b" 
+conan create "${SCRIPT_DIR}/lib_a" 
 
-conan create "${SCRIPT_DIR}/lib_a" -b missing
-conan create "${SCRIPT_DIR}/lib_a" -b missing -o *:shared=True
+conan create "${SCRIPT_DIR}/lib_c"-o *:shared=True
+conan create "${SCRIPT_DIR}/lib_b"-o *:shared=True
+conan create "${SCRIPT_DIR}/lib_a"-o *:shared=True
