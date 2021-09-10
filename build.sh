970b03c9824dd7cdd6a1900b1d2b30aa0c636030
ericLemanissier <ericLemanissier@users.noreply.github.com>
2021-09-10 12:42:11 +0200

    Apply suggestions from code review
    Co-authored-by: Florian Berchtold <florian.berchtold@luminartech.com>

--- build.sh
+++ build.sh
@@ -4,10 +4,10 @@ set -euxo pipefail
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-conan create "${SCRIPT_DIR}/lib_c" 
-conan create "${SCRIPT_DIR}/lib_b" 
-conan create "${SCRIPT_DIR}/lib_a" 
+conan create "${SCRIPT_DIR}/lib_c"
+conan create "${SCRIPT_DIR}/lib_b"
+conan create "${SCRIPT_DIR}/lib_a"
 
-conan create "${SCRIPT_DIR}/lib_c"-o *:shared=True
-conan create "${SCRIPT_DIR}/lib_b"-o *:shared=True
-conan create "${SCRIPT_DIR}/lib_a"-o *:shared=True
+conan create "${SCRIPT_DIR}/lib_c" -o *:shared=True
+conan create "${SCRIPT_DIR}/lib_b" -o *:shared=True
+conan create "${SCRIPT_DIR}/lib_a" -o *:shared=True
